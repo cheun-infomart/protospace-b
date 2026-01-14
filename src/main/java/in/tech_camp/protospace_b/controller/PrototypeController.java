e34b1d6de8c94ee0d84cd770d9847737a1df3687
RyoyaInfom <r.nakanouchi@infomart.co.jp>
2026-01-14 11:27:12 +0900

    コメントエリア以外のプロトタイプ詳細機能追加

---
+++ src/main/java/in/tech_camp/protospace_b/controller/PrototypeController.java
@@ -0,0 +1,28 @@
+package in.tech_camp.protospace_b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import in.tech_camp.protospace_b.entity.PrototypeEntity;
+import in.tech_camp.protospace_b.entity.UserEntity;
+import in.tech_camp.protospace_b.repository.PrototypeRepository;
+import in.tech_camp.protospace_b.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class PrototypeController {
+  private final PrototypeRepository prototypeRepository;
+  private final UserRepository userRepository;
+
+  @GetMapping("/prototypes/{prototypeId}")
+  public String showTweetDetail(@PathVariable("prototypeId") Integer prototypeId, Model model) {
+      PrototypeEntity prototype = prototypeRepository.findById(prototypeId);
+      UserEntity user = userRepository.findById(prototype.getUserId());
+      model.addAttribute("prototype", prototype);
+      model.addAttribute("user", user);
+      return "prototypes/show";
+  }
+}
---
+++ src/main/java/in/tech_camp/protospace_b/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package in.tech_camp.protospace_b.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import in.tech_camp.protospace_b.entity.UserEntity;
+
+@Mapper
+public interface UserRepository {
+  @Select("SELECT * FROM users WHERE id = #{id}")
+  UserEntity findById(Integer id);
+} 
